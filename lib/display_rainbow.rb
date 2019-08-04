296c77c67714ed3c10eefe7e3a04812aa5c0eef5
Patrick Alexander <patrick_alexander@mail.com>
2019-08-04 16:58:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-colors = ['R: red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+colors = ['R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet']
 
 def display_rainbow(colors)
   puts colors
