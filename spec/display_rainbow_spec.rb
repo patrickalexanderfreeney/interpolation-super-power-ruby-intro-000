37b4aa28a47e832b76e9d137e0acd634d1f76d82
Patrick Alexander <patrick_alexander@mail.com>
2019-08-04 17:00:42 +0000

    Automatically backed up by Learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -1,6 +1,6 @@
 describe '#display_rainbow' do
   it 'accepts one argument' do
-    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    colors = ['R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet']
 
     allow(self).to receive(:puts)
 
@@ -9,15 +9,15 @@ describe '#display_rainbow' do
   end
 
   it 'prints out the colors of the rainbow correctly when passed in in order' do
-    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    colors = ['R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet']
 
-    expect(colors).to receive(:[]).with(0).at_least(:once).and_return("red")
-    expect(colors).to receive(:[]).with(1).at_least(:once).and_return("orange")
-    expect(colors).to receive(:[]).with(2).at_least(:once).and_return("yellow")
-    expect(colors).to receive(:[]).with(3).at_least(:once).and_return("green")
-    expect(colors).to receive(:[]).with(4).at_least(:once).and_return("blue")
-    expect(colors).to receive(:[]).with(5).at_least(:once).and_return("indigo")
-    expect(colors).to receive(:[]).with(6).at_least(:once).and_return("violet")
+    expect(colors).to receive(:[]).with(0).at_least(:once).and_return("R: red")
+    expect(colors).to receive(:[]).with(1).at_least(:once).and_return("O: orange")
+    expect(colors).to receive(:[]).with(2).at_least(:once).and_return("Y: yellow")
+    expect(colors).to receive(:[]).with(3).at_least(:once).and_return("G: green")
+    expect(colors).to receive(:[]).with(4).at_least(:once).and_return("B: blue")
+    expect(colors).to receive(:[]).with(5).at_least(:once).and_return("I: indigo")
+    expect(colors).to receive(:[]).with(6).at_least(:once).and_return("v: violet")
 
     expect { display_rainbow(colors) }.to output("R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n").to_stdout
   end
